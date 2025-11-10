c161575aefb2c89927cbb872d1a8673f2591a500
azzaaz-bit <azzafathima164@gmail.com>
2025-11-10 16:06:38 +0530

    updated portfolio layout

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// script.js
+// Toggle mobile menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  menuToggle.classList.toggle('active');
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navLinks.classList.toggle("active");
 });
